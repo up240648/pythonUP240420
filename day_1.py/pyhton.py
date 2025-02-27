fd1a4dc61a43b82ac95040cc9163cce1b1d87a74
UP240420 <up240420@alumnos.upa.edu.mx>
2025-02-27 12:45:34 -0600

    edit

---
+++ day_1.py/pyhton.py
@@ -0,0 +1,22 @@
+# Day 1 
+
+print(3 + 2)  
+print(3 - 2)  
+print(3 * 2)   
+print(3 / 2)  
+print(3 ** 2)  
+print(3 % 2)   
+print(3 // 2)  
+
+# Checking data types
+
+print(type(10))                  
+print(type(3.14))                
+print(type(1 + 3j))             
+print(type('everado'))          
+print(type([1, 2, 3]))          
+print(type({'everardo':'venegas'})) 
+print(type({9.8, 3.14, 2.7}))    
+print(type((9.8, 3.14, 2.7)))   
+print(type(3 == 3))             
+print(type(3 >= 3))             
