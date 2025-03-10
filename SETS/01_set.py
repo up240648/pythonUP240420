e608a5066ed738237e59ab49dc307437f0a460c2
UP240468 <up240463@alumnos.upa.edu.mx>
2025-03-10 13:30:06 -0600

    cambio

---
+++ SETS/01_set.py
@@ -0,0 +1,17 @@
+# sets
+my_set = set()
+my_set_01 = {}
+
+my_set = {"EVER", "venegas", " 240420"}
+print(type(my_set))
+print(type(my_set_01))
+
+my_set_01 = set(["upa", "mtr"])
+print(type(my_set_01))
+
+my_set.add("hernandez")
+print(my_set)
+
+my_set.add("hernandez")
+my_set.remove("hernandez")
+print(my_set)
